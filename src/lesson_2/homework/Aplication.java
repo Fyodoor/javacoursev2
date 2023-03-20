02416ddb6dd039eb2ae24141cb38eb028f4d784e
Knyaz <rewppp6@gmail.com>
2023-03-20 19:26:26 +0200

    first four

---
+++ src/lesson_2/homework/Aplication.java
@@ -0,0 +1,15 @@
+package lesson_2.homework;
+
+public class Aplication {
+    public static int a = 1;
+    public static int b = 3;
+    public static int c = 9;
+    public static int d = 27;
+
+    public static void main(String[] args) {
+
+        int result = -a + b - c + d;
+
+        System.out.println(result);
+    }
+}
