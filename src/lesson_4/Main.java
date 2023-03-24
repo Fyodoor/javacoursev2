f348c8f03fdf7dafef8019fb0c8200bf8b3d542d
Knyaz <rewppp6@gmail.com>
2023-03-24 23:19:35 +0200

    конспект 4 урок

---
+++ src/lesson_4/Main.java
@@ -0,0 +1,77 @@
+package lesson_4;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        // for i
+//        int a = 0;
+//        a++;
+        // Объявление переменной i - означает порог от которого мы будем  отсчитывать
+        // Вторая часть - условие выхода из цикла
+        // Третья чаас - инкрементирование переменной i
+//        for (int i = 0 ; i <= 5; i++){
+//            System.out.println("Hello world!" + " " + i);
+//        }
+
+//        for (int i = 1; i <= 100 ; i++) {
+//            System.out.println(i);
+//        }
+
+//         for (int j = 1; j <= 100; j++) {
+//             if ( j % 2 == 0) {
+//                 System.out.println(j);
+//             }
+//         }
+
+//        for (int i = 50; i <= 150 ; i++) {
+//            if ( i % 2 != 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+        // делятся на 3 и 5 одновременно от 0 да 150
+
+//        for (int i = 0; i <= 150; i++) {
+//            if (i % 15 == 0 && i != 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+        //сложить цифры в диапозоне от 10 до 20
+//        int sum = 0;
+//
+//        for (int i = 10; i <= 20 ; i++) {
+//            sum += i;
+//        }
+//        System.out.println(sum);
+
+        // вывести в консоль сумму всех четных чисел от 10 до 50
+//        int sum = 0;
+//        for (int i = 10; i <= 50; i++) {
+//            if ( i % 2 == 0) {
+//                sum += i;
+//            }
+//        }
+//        System.out.println(sum);
+
+        // все числа от 10 до 40, пропустив при этом числа от 15 до 20
+
+//        for (int i = 10; i <= 40; i++) {
+//            if (i < 15 || i > 20) {
+//                System.out.println(i);
+//            }
+//        }
+
+
+        // while
+//        int i = 10;
+//        System.out.println("Final countdown....");
+//        while (i > 0) {
+//            Thread.sleep(1000);
+//            System.out.println(i);
+//            i--;
+//        }
+//        System.out.println("BOOOM!!!");
+
+    }
+}
