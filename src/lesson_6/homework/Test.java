dd657228e9c9ace2b4cfc7c6dbe7a57b31e1b412
Knyaz <rewppp6@gmail.com>
2023-04-02 20:44:18 +0300

    5 задача от 30.03.2023. Более корректная версия

---
+++ src/lesson_6/homework/Test.java
@@ -0,0 +1,29 @@
+package lesson_6.homework;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] linesArray = new String[6];
+        System.out.println("Введите 6 строк");
+        for (int i = 0; i < linesArray.length; i++) {
+            linesArray[i] = scanner.nextLine();
+        }
+        for (int i = 0; i < linesArray.length; i++) {
+            for (int j = 0; j < linesArray.length; j++) {
+                if (linesArray[i] != null && i != j) {
+                    if (linesArray[i].equals(linesArray[j])) {
+                        linesArray[j] = null;
+                    }
+                }
+            }
+        }
+
+        for (String c : linesArray) {
+
+            System.out.println(c);
+
+        }
+    }
+}
